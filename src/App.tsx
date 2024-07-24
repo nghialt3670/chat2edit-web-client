3e09f777e573dccdea2da4676435abbd2da1cf98
nghialt3670 <nghialt3670@gmail.com>
2024-07-25 01:38:34 +0700

    Add base_url

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Edit from "./pages/Edit";
+import { base_url } from "./config";
 
 function App() {
   return (
-    <BrowserRouter >
+    <BrowserRouter basename={base_url}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/edit" element={<Edit />} />
